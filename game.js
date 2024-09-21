a13fdff0be2b82e68c09748ab2c82c83e1b27fca
jafetegill17 <106451813+largefort@users.noreply.github.com>
2024-09-21 19:10:20 +0000

    Update game.js

--- game.js
+++ game.js
@@ -282,7 +282,7 @@ setInterval(saveGame, 10000);
 // Service Worker Registration
 if ('serviceWorker' in navigator) {
     window.addEventListener('load', () => {
-        navigator.serviceWorker.register('/service-worker.js')
+        navigator.serviceWorker.register('https://elementaltycoon.fjordrgames.online/service-worker.js', { scope: 'https://elementaltycoon.fjordrgames.online/' })
             .then((registration) => {
                 console.log('Service Worker registered with scope:', registration.scope);
             })
