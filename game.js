9c054a7f13481ba3ef5211ea3ed627e638217a47
jafetegill17 <106451813+largefort@users.noreply.github.com>
2024-09-21 19:06:04 +0000

    Update game.js

--- game.js
+++ game.js
@@ -279,6 +279,19 @@ function switchVersion(version) {
 
 setInterval(saveGame, 10000);
 
+// Service Worker Registration
+if ('serviceWorker' in navigator) {
+    window.addEventListener('load', () => {
+        navigator.serviceWorker.register('/service-worker.js')
+            .then((registration) => {
+                console.log('Service Worker registered with scope:', registration.scope);
+            })
+            .catch((error) => {
+                console.log('Service Worker registration failed:', error);
+            });
+    });
+}
+
 window.onload = () => {
     loadGame();
     generateRandomUpgrade();
